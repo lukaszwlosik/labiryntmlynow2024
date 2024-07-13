e63f2886444d25279dc628676f5a5f58b2359167
Lucas <109286279+lukaszwlosik@users.noreply.github.com>
2024-07-13 14:35:28 +0200

    Delete js directory

--- js/mobile-bar.js
+++
@@ -1,8 +0,0 @@
-let mobileMenu = document.querySelector(".mobile-menu");
-let homepageH1 = document.getElementById("h1-homepage");
-
-function toggleMobileMenu(menu) {
-  menu.classList.toggle("open");
-  mobileMenu.classList.toggle("hidden");
-  homepageH1.classList.toggle("hidden");
-}
